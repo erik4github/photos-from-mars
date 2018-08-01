8896a44c59fba491c5bb920b999b00b82fd3b583
unknown <erik4github@gmail.com>
2018-07-31 20:19:51 -0500

    Remove comments

--- src/components/api/getNewPhotos.js
+++ src/components/api/getNewPhotos.js
@@ -1,21 +1,3 @@
-// if using own server to hide API key
-// const endpointURL = '';
-// export function getNewPhotos(formData) {
-//     let photos;
-//     return fetch(endpointURL, {
-//             method: 'POST',
-//             body: formData,
-//             headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-//             }
-//         })
-//         .then((response) => response.ok ? response.json() : console.log(response.status))
-//         .then((responseJSON) => {
-//             photos = responseJSON.photos;
-//             return photos;
-//         });
-// }
-
 import { httpsConverter } from '../utility/utility';
 
 export function getNewPhotos(rover, sol) {
