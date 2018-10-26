d8934bbb2dc5cc59fdd30760cb702c3513ef9364
unknown <erik4github@gmail.com>
2018-10-26 14:17:38 -0500

    Fix latest photos returning null response sometimes

--- src/components/api/getLatestPhotos.js
+++ src/components/api/getLatestPhotos.js
@@ -3,7 +3,8 @@ import { httpsConverter } from '../utility/utility';
 export function getLatestPhotos() {
     let photos;
     return fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/latest_photos`)
-        .then((response) => response.ok ? response.json() : console.log(response.status))
+        .then((response) => response.text())
+        .then((text) => text.length ? JSON.parse(text) : {}) 
         .then((responseJSON) => {
             let convertedJSON = JSON.stringify(responseJSON, httpsConverter);
             convertedJSON = JSON.parse(convertedJSON);
